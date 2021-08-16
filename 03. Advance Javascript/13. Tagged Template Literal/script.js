7f6d0e29165c7d171f225e2cb0518c997231bd09
Prasetiyo <prasetiyo2020@yahoo.com>
2021-08-16 19:54:02 +0700

    menambah file

---
+++ 03. Advance Javascript/13. Tagged Template Literal/script.js
@@ -0,0 +1,12 @@
+// Tagged Template Literal
+/*
+"Bentuk yang lebih kompleks dari template Literals, memungkinkan kita untuk membaca template literals melalui sebuah function." - MDN Web Docs
+*/
+
+const nama = 'Prasetiyo';
+const umur = 23;
+
+
+
+const str = coba`Halo, nama saya ${nama}, saya ${umur} tahun.`;
+console.log(str);
