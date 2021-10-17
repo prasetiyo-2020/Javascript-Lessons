6497255d41563df80d4b055dcf0ab2cc8a4d364c
Prasetiyo <84963363+prasetiyo-2020@users.noreply.github.com>
2021-10-17 20:44:49 +0700

    Update 03. Variabel.js

--- 01. Basic Javascript/03. Variabel.js
+++ 01. Basic Javascript/03. Variabel.js
@@ -2,11 +2,14 @@
 var lastName;
 lastName = "Prasetiyo";
 console.log(lastName);
+// Output : Prasetiyo
 
 // Deklarasi variabel dengan let
 let lastName = "Prasetiyo";
 console.log(lastName);
+// Output : Prasetiyo
 
 // Deklarasi variabel dengan const
 const lastName = "Prasetiyo";
 console.log(lastName);
+// Output : Prasetiyo
