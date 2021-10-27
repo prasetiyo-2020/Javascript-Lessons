cabc8fb1fde4c5780987d777d3c622e1290e27af
Prasetiyo <84963363+prasetiyo-2020@users.noreply.github.com>
2021-10-27 11:42:31 +0700

    Create Konversi Waktu.js

---
+++ 04. Case Study/06. Matematika/Konversi Waktu.js
@@ -0,0 +1,65 @@
+
+// Konversi Menit ke Detik
+const menitKeDetik = function (menit) {
+    return menit * 60;
+}
+
+// Konversi Jam ke Menit
+const jamKeMenit = function (jam) {
+    return jam * 60;
+}
+
+// Konversi Hari ke Jam
+const hariKeJam = function (hari) {
+    return hari * 24;
+}
+
+// Konversi Minggu ke Hari
+const mingguKeHari = function (minggu) {
+    return minggu * 7;
+}
+
+// Konversi Bulan ke Hari
+const bulanKeHari = function (bulan) {
+    return bulan * 30;
+}
+
+// Konversi Bulan ke Minggu
+const bulanKeMinggu = function (bulan) {
+    return bulan * 4;
+}
+
+// Konversi Tahun ke Bulan
+const tahunKeBulan = function (tahun) {
+    return tahun * 12;
+}
+
+// Konversi Tahun ke Minggu
+const tahunKeMinggu = function (tahun) {
+    return tahun * 48;
+}
+
+// Konversi Windu ke Tahun
+const winduKeTahun = function (windu) {
+    return windu * 8;
+}
+
+// Konversi Dekade ke Tahun
+const dekadeKeTahun = function (dekade) {
+    return dekade * 10;
+}
+
+// Konversi Dasawarsa ke Tahun
+const dasawarsaKeTahun = function (dasawarsa) {
+    return dasawarsa * 10;
+}
+
+// Konversi Abad ke Tahun
+const abadKeTahun = function (abad) {
+    return abad * 100;
+}
+
+// Konversi Milenium ke Tahun
+const mileniumKeTahun = function (milenium) {
+    return milenium * 1000;
+} 
